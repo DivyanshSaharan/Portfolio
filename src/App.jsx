166d19ec3ae8732e72ec0862c25595421bb3f6c7
DivyanshSaharan <divyanshsaharan2@gmail.com>
2024-06-16 20:02:12 +0530

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import About from './Components/About/About';
 function App() {
   return (
     <>
       <Header />
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   )
